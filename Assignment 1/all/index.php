741e40fc186b5b962f445eccdd01c8148a2e2a16
ronaldalegonzalez-spec <ronaldalegonzalez@gmail.com>
2025-11-01 13:09:59 -0400

    Add the the title and links for icon

---
+++ Assignment 1/all/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="Pizzeria" content="Info about ARAM' PIZZA PIZZERIA and options to order pizza online">
+        <link rel="shortcut icon" href="images/ARAM PIZZA ICON.jpg" type="image/x-icon" />
+        <link rel="stylesheet" href="css/stylish.css" />
+        <title>ARAM' PIZZA PIZZERIA</title>
+    </head>
+        <body>
+            <header>
+            <h1>Welcome to ARAM' PIZZA PIZZERIA</h1>
+            </header>
+            <h2>Your favorite pizza place!</h2>
+            <a href="../login.php">Login</a> | 
+            <a href="../register.php">Register</a>
